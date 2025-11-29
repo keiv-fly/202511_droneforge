48183785f4a054a5417cb7a53776964568c64e11
Sergey N <keiv84@example.com>
2025-11-29 11:27:32 +0300

    Checking that deployment works

--- droneforge-web/src/lib.rs
+++ droneforge-web/src/lib.rs
@@ -23,6 +23,13 @@ impl GameState {
             24.0,
             WHITE,
         );
+        draw_text(
+            &format!("Hello"),
+            20.0,
+            80.0,
+            24.0,
+            WHITE,
+        );
     }
 }
 
